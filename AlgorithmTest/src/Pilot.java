dd2a7010ba561b0bac358c3fd44ecc404c8e45b4
Gabriel Begun <begunbegun@gmail.com>
2014-03-28 15:05:55 -0400

    Update PIlot to work with UART

--- AlgorithmTest/src/Pilot.java
+++ AlgorithmTest/src/Pilot.java
@@ -2,7 +2,11 @@
 //import java.util.ArrayList;
 import java.util.Queue;
 import java.util.LinkedList;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.*;
+import javax.comm.CommPortIdentifier;
 
 public class Pilot{
   public Pilot() { }
@@ -15,12 +19,17 @@ public class Pilot{
   private int battery;
   private int compass;
   
+  private static boolean sendingMessage;
+  
   private boolean sendMessage() {
+    System.out.println("Messsage to be sent:");
     String str = (String.format("s%02d",messageQueue.size()));
-    //Send to UART
     System.out.println(str);
     for(int ii = 0; ii <= messageQueue.size(); ii++)
       System.out.println(messageQueue.poll());
+    sendingMessage = true;
+    while(sendingMessage) {System.out.println("SENDING MESSAGE"); }
+    System.out.println("DONE SENDING");
     return true;
   }
   
@@ -54,8 +63,94 @@ public class Pilot{
     return success;
   }
   
+  
+  // CODE FOR UART COMMUNICATION
+ void connect( String portName ) throws Exception {
+    CommPortIdentifier portIdentifier = CommPortIdentifier
+        .getPortIdentifier( portName );
+    if( portIdentifier.isCurrentlyOwned() ) {
+      System.out.println( "Error: Port is currently in use" );
+    } else {
+      int timeout = 2000;
+      CommPort commPort = portIdentifier.open( this.getClass().getName(), timeout );
+ 
+      if( commPort instanceof SerialPort ) {
+        SerialPort serialPort = ( SerialPort )commPort;
+        serialPort.setSerialPortParams( 9600,
+                                        SerialPort.DATABITS_8,
+                                        SerialPort.STOPBITS_1,
+                                        SerialPort.PARITY_NONE );
+ 
+        InputStream in = serialPort.getInputStream();
+        OutputStream out = serialPort.getOutputStream();
+ 
+        ( new Thread( new SerialReader( in ) ) ).start();
+        ( new Thread( new SerialWriter( out ) ) ).start();
+ 
+      } else {
+        System.out.println( "Error: Only serial ports are handled by this example." );
+      }
+    }
+  }
+ 
+  public static class SerialReader implements Runnable {
+ 
+    InputStream in;
+ 
+    public SerialReader( InputStream in ) {
+      this.in = in;
+    }
+ 
+    public void run() {
+      byte[] buffer = new byte[ 1024 ];
+      int len = -1;
+      try {
+        while( ( len = this.in.read( buffer ) ) > -1 ) {
+          System.out.print( new String( buffer, 0, len ) );
+        }
+      } catch( IOException e ) {
+        e.printStackTrace();
+      }
+    }
+  }
+ 
+  public static class SerialWriter implements Runnable {
+ 
+    OutputStream out;
+ 
+    public SerialWriter( OutputStream out ) {
+      this.out = out;
+    }
+ 
+    public void run() {
+      try {
+        while(!sendingMessage) {
+         String str = (String.format("s%02d",messageQueue.size()));
+         for(int ii = 0; ii <= messageQueue.size(); ii++)
+           str += messageQueue.poll();
+
+          this.out.write(str.getBytes());
+        }
+        sendingMessage = false;
+/*        int c = 0;
+        while( ( c = System.in.read() ) &gt; -1 ) {
+          this.out.write( c );
+        } */
+      } catch( IOException e ) {
+        e.printStackTrace();
+      }
+    }
+  } 
+  
+  
  public static void main (String[] args) {
   Pilot pilot = new Pilot();
+  try {
+  pilot.connect("/dev/ttyUSB0" );
+  } catch (Exception ex) {
+    System.out.println("Could not open tty, exiting");
+    return;
+  }
   pilot.setArmed(1);
   pilot.sync();
   
@@ -76,3 +171,89 @@ public class Pilot{
   pilot.powerOff();
  } 
 }
+
+
+
+/* import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+ 
+public class TwoWaySerialComm {
+ 
+  void connect( String portName ) throws Exception {
+    CommPortIdentifier portIdentifier = CommPortIdentifier
+        .getPortIdentifier( portName );
+    if( portIdentifier.isCurrentlyOwned() ) {
+      System.out.println( "Error: Port is currently in use" );
+    } else {
+      int timeout = 2000;
+      CommPort commPort = portIdentifier.open( this.getClass().getName(), timeout );
+ 
+      if( commPort instanceof SerialPort ) {
+        SerialPort serialPort = ( SerialPort )commPort;
+        serialPort.setSerialPortParams( 57600,
+                                        SerialPort.DATABITS_8,
+                                        SerialPort.STOPBITS_1,
+                                        SerialPort.PARITY_NONE );
+ 
+        InputStream in = serialPort.getInputStream();
+        OutputStream out = serialPort.getOutputStream();
+ 
+        ( new Thread( new SerialReader( in ) ) ).start();
+        ( new Thread( new SerialWriter( out ) ) ).start();
+ 
+      } else {
+        System.out.println( "Error: Only serial ports are handled by this example." );
+      }
+    }
+  }
+ 
+  public static class SerialReader implements Runnable {
+ 
+    InputStream in;
+ 
+    public SerialReader( InputStream in ) {
+      this.in = in;
+    }
+ 
+    public void run() {
+      byte[] buffer = new byte[ 1024 ];
+      int len = -1;
+      try {
+        while( ( len = this.in.read( buffer ) ) &gt; -1 ) {
+          System.out.print( new String( buffer, 0, len ) );
+        }
+      } catch( IOException e ) {
+        e.printStackTrace();
+      }
+    }
+  }
+ 
+  public static class SerialWriter implements Runnable {
+ 
+    OutputStream out;
+ 
+    public SerialWriter( OutputStream out ) {
+      this.out = out;
+    }
+ 
+    public void run() {
+      try {
+        int c = 0;
+        while( ( c = System.in.read() ) &gt; -1 ) {
+          this.out.write( c );
+        }
+      } catch( IOException e ) {
+        e.printStackTrace();
+      }
+    }
+  }
+ 
+  public static void main( String[] args ) {
+    try {
+      ( new TwoWaySerialComm() ).connect( "/dev/ttyUSB0" );
+    } catch( Exception e ) {
+      e.printStackTrace();
+    }
+  }
+} */
