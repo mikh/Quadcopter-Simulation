d467dad26bc3a3f5646a82086c8c4a0c14736705
MikhailAndreev <mikha.andreev@gmail.com>
2014-03-29 22:27:27 -0400

    Added run scripts

---
+++ AlgorithmTest/src/compile_pilot.sh
@@ -0,0 +1,2 @@
+rm *.class
+javac -cp /usr/share/java/rxtx.jar Pilot.java
---
+++ AlgorithmTest/src/run_pilot.sh
@@ -0,0 +1,1 @@
+java -cp .:/usr/share/java/rxtx.jar -Djava.library.path=/usr/lib/jni/ -Dgnu.io.rxtx.SerialPorts=/dev/ttyO4 Pilot
